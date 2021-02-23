923f436b5e3e80f733b986cc5b3153e41435a529
root <rajesh.allun@gmail.com>
2021-02-23 12:30:23 +0000

    Add redis config

---
+++ k8sdemo-twotier/web/main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from redis import Redis
+
+app = Flask(__name__)
+redis = Redis(host='redis-k8s', port=6379)
+
+@app.route("/")
+def main():
+    redis.incr('hits')
+    count = int(redis.get('hits'))
+    return render_template('main.html', hits=count)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
